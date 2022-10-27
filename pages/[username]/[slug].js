5e63f584f339d98198cc22687d56e22bd40197a9
Nick Dalby <nckdlby@gmail.com>
2022-10-27 17:26:12 +0200

    post content page, SSR + ISR

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,4 +1,80 @@
-const PostPage = () => {
-  return <div>PostPage</div>
+import styles from '@styles/Post.module.css';
+
+import Link from 'next/link'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { useContext } from 'react'
+import { getUserWithUsername, postToJSON } from '@lib/firebase'
+import {
+  doc,
+  getDocs,
+  getDoc,
+  collectionGroup,
+  query,
+  limit,
+  getFirestore,
+} from 'firebase/firestore'
+import PostContent from '@components/PostContent';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params
+  const userDoc = await getUserWithUsername(username)
+
+  let post
+  let path
+
+  if (userDoc) {
+    const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug)
+
+    post = postToJSON(await getDoc(postRef))
+
+    path = postRef.path
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 100,
+  }
 }
-export default PostPage
+
+export async function getStaticPaths() {
+  const q = query(collectionGroup(getFirestore(), 'posts'), limit(20))
+  const snapshot = await getDocs(q)
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data()
+    return {
+      params: { username, slug },
+    }
+  })
+
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+const Post = (props) => {
+  const postRef = doc(getFirestore(), props.path)
+  const [realtimePost] = useDocumentData(postRef)
+
+  const post = realtimePost || props.post
+
+  return (
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post}/>
+      </section>
+      <aside className='card'>
+        <p>
+          <strong>{post.heartCount || 0} ❤️</strong>
+        </p>
+      </aside>
+
+    </main>
+  )
+}
+export default Post
