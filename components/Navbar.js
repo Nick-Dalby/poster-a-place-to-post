3677586e7d235fb1491e82fd4a3ac361c6a104de
Nick Dalby <nckdlby@gmail.com>
2022-10-28 10:43:28 +0200

    navbar sign-out

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,20 @@
+import { UserContext } from '@lib/context'
+import { auth } from '@lib/firebase'
+import { signOut } from 'firebase/auth'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useContext } from 'react'
-import { UserContext } from '@lib/context'
 
 const Navbar = () => {
   const { user, username } = useContext(UserContext)
+
+  const router = useRouter();
+
+  const signOutNow = () => {
+    signOut(auth);
+    router.reload();
+  }
 
   return (
     <nav className="navbar">
@@ -17,10 +27,10 @@ const Navbar = () => {
         {/* user signed in with username */}
         {username && (
           <>
-           <li className="push-left">
-              <button>Sign Out</button>
+            <li className="push-left">
+              <button onClick={signOutNow}>Sign Out</button>
             </li>
-            <li >
+            <li>
               <Link href="/admin">
                 <button className="btn-blue">Write Posts</button>
               </Link>
